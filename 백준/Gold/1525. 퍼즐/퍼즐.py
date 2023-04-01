b5ed7871469077f7da6ee6c512bba3bab3c9f90f
Jeyong <118044367+joon6093@users.noreply.github.com>
2023-04-01 18:16:49 +0900

    [Gold II] Title: 퍼즐, Time: 436 ms, Memory: 170088 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.py
+++ 백준/Gold/1525. 퍼즐/퍼즐.py
@@ -1,6 +1,5 @@
 import sys
 from collections import deque
-import copy
 input = sys.stdin.readline
 
 
@@ -11,7 +10,7 @@ def bfs(puzzle):
         puzzle=queue.popleft()
         if puzzle == "123456780":
             return visited["123456780"]
-        pos = puzzle.index('0')
+        pos = puzzle.index('0') 
         row=pos//3
         col=pos%3
         for x,y in move:
