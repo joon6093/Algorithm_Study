e05b88c6088bd3aafbfb2087c624e3cdac04bc61
Jeyong <118044367+joon6093@users.noreply.github.com>
2023-04-05 01:50:28 +0900

    [Silver V] Title: 세 부분, Time: 60 ms, Memory: 33300 KB -BaekjoonHub

---
+++ 백준/Silver/2993. 세 부분/세 부분.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N=input().strip()
+
+word_list=[]
+for i in range(len(N)-2):
+    for j in range(i+1, len(N)-1):
+        for k in range(j+1, len(N)):
+            word = N[:j][::-1] + N[j:k][::-1] + N[k:][::-1]
+            word_list.append(word)
+
+print(min(word_list))
