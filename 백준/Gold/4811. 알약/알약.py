6a657b3d02e450f3d195af300c01c585d4835a2a
jeyong <118044367+joon6093@users.noreply.github.com>
2024-03-15 01:22:49 +0900

    [Gold V] Title: 알약, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+dp = [[0]*31 for _ in range(31)]
+for i in range(31):
+    dp[0][i]=1 
+
+for i in range(1,31):
+    for j in range(i,31):
+        dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        
+N = int(input().strip())
+while N  != 0:
+    print(dp[N][N])
+    N = int(input().strip())
+
