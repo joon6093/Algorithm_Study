10624e4e4ae8db7a3a10ef75360ea85c4622889c
jeyong <118044367+joon6093@users.noreply.github.com>
2024-10-16 01:52:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,7 +1,8 @@
 def solution(n):
-    dp = [0] * (n + 2)
+    dp = [0] * (n + 1)
     dp[1] = 1
-    dp[2] = 2
+    if n > 1:
+        dp[2] = 2
     
     for i in range(3,n + 1):
         dp[i] = dp[i-2] + dp[i-1]
