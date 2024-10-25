26622c049309045a66e012f682cf202cd07d9405
jeyong <118044367+joon6093@users.noreply.github.com>
2024-10-26 03:13:57 +0900

    [level 2] Title: 땅따먹기, Time: 574.34 ms, Memory: 43.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,11 @@
+def solution(land):
+    dp = [[0] * 4 for _ in range(len(land))]
+    dp[0] = land[0]
+    
+    for i in range(1, len(land)):
+        for j in range(4):
+            for k in range(4):
+                if j != k:
+                    dp[i][j] = max(dp[i][j], dp[i-1][k] + land[i][j])
+    
+    return max(dp[-1])
