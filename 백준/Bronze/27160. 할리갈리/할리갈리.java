f6a18e20993fd3c766fac0fa6f2146a230bdcc7a
jeyong <118044367+joon6093@users.noreply.github.com>
2024-11-02 19:43:14 +0900

    [Bronze II] Title: 할리갈리, Time: 436 ms, Memory: 50076 KB -BaekjoonHub

---
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(" ");
+            if (map.containsKey(split[0])) {
+                map.put(split[0], map.get(split[0]) + Integer.parseInt(split[1]));
+            } else {
+                map.put(split[0], Integer.parseInt(split[1]));
+            }
+        }
+
+        if (map.containsValue(5)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
