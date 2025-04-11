71ce9692708b68a98481a62418764397e2504be6
jeyong <joon6093@naver.com>
2025-04-11 19:06:09 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.87 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+class Solution {
+
+    public int[] solution(
+        int k,
+        int[] score
+    ) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+            
+            answer[i] = priorityQueue.peek();
+        }
+
+        return answer;
+    }
+}
