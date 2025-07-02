4ef5cd253028fdc7d835d8687e62620143bd0d60
jeyong <joon6093@naver.com>
2025-07-02 21:48:22 +0900

    [Silver III] Title: 가희와 키워드, Time: 1072 ms, Memory: 219132 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String[] s = br.readLine().split(",");
+            for (final String string : s) {
+                set.remove(string);
+            }
+            bw.write(String.valueOf(set.size()));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
