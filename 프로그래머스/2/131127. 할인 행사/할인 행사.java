ece3b48bcaa5975fd30d055a7128ecf56806ac3b
jeyong <joon6093@naver.com>
2025-08-03 01:55:16 +0900

    [level 2] Title: 할인 행사, Time: 51.49 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int solution(
+        String[] want,
+        int[] number,
+        String[] discount
+    ) {
+        int answer = 0;
+        for (int i = 0; i <= discount.length - 10; i++) {
+            int[] count = new int[want.length];
+            for (int j = i; j < i + 10; j++) {
+                for (int k = 0; k < want.length; k++) {
+                    if (discount[j].equals(want[k])) {
+                        count[k]++;
+                    }
+                }
+            }
+            boolean isValid = true;
+            for (int k = 0; k < want.length; k++) {
+                if (count[k] < number[k]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
