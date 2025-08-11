402a1718a4ab9b716891ad70b97dcadbb85a3119
jeyong <joon6093@naver.com>
2025-08-12 02:37:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 84884 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int movieName = 666;
-        int count = 1;
-
-        while (N > count) {
-            movieName++;
-
-            if (String.valueOf(movieName)
-                .contains("666")) {
-                count++;
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int num = 666;
+        while (true) {
+            if (Integer.toString(num).contains("666")) {
+                N--;
+                if (N == 0) {
+                    bufferedWriter.write(Integer.toString(num));
+                    bufferedWriter.flush();
+                    break;
+                }
             }
+            num++;
         }
-
-        System.out.println(movieName);
     }
 }
