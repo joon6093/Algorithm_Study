7777798e4e17987a6d3c26b4d81ab277aad4ed35
jeyong <joon6093@naver.com>
2025-08-28 14:02:35 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        int[] P = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int arr[] = new int[N];
+        String str[] = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            P[i] = (Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(str[i]);
         }
 
-        Arrays.sort(P);
-        int sum = 0;
+        Arrays.sort(arr);
         int answer = 0;
-        for (int hour : P) {
-            sum += hour;
-            answer += sum;
-        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer += arr[j];
+            }
 
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        br.close();
-        bw.close();
+        }
+        
+        System.out.println(answer);
     }
 }
