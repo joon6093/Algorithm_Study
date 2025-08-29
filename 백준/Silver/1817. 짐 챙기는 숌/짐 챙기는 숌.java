84a9c3f8f3e660d3def77b62ef4a1a7140a4f1a2
jeyong <joon6093@naver.com>
2025-08-30 02:44:17 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 1;
+        int current = M;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] <= current) {
+                current -= arr[i];
+            } else {
+                result++;
+                current = M - arr[i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
